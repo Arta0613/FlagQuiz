aa4ecc74c6f99e4828a4d9717d15281911ba9564
Mohammed Amin <arta0613@gmail.com>
2016-05-01 16:52:31 -0400

    Add SettingsActivity/Fragment; squash some bugs

--- app/src/main/java/com/rushlimit/flagquiz/SettingsActivityFragment.java
+++ app/src/main/java/com/rushlimit/flagquiz/SettingsActivityFragment.java
@@ -1,22 +1,17 @@
 package com.rushlimit.flagquiz;
 
+import android.preference.PreferenceFragment;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A placeholder fragment containing a simple view.
- */
-public class SettingsActivityFragment extends Fragment {
-
-    public SettingsActivityFragment() {
-    }
-
+public class SettingsActivityFragment extends PreferenceFragment {
+    // Creates preferences GUI from preferences.xml file in res/xml
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
     }
 }
